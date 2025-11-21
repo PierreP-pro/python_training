ef84365ab9864bc46761aa1a25e57c26df682e18
PierreP-pro <pierrepalazin@gmail.com>
2025-11-21 17:57:38 +0100

    New changes

--- first_program.py
+++ first_program.py
@@ -1,1 +1,30 @@
-print("Hello, world!", "how's it going?")
+"""
+import time
+
+phrase = "I don't know, but it seems cloudy today."
+
+finded = phrase.find("do")
+
+print(finded)
+
+print("do" in phrase)
+
+
+print(time.time())
+"""
+
+import re
+
+
+zip_code = r"\d{5}"
+
+search = re.search(zip_code, "Beverly Hills 90210")
+
+if search:
+    print("It's got a ZIP code!")
+else:
+    print("No match!")
+
+
+
+print(search)
